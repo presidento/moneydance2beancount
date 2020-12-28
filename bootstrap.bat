010b222f694a693a9e103e3f327fee2f5216b940
Máté Farkas <mate.farkas@oneidentity.com>
2020-12-28 14:28:17 +0100

    Rewrite bootstrap from batch to Powershelll

--- bootstrap.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-IF NOT EXIST .venv\ py -3.7 -m venv .venv --prompt md2bean
-call .venv\Scripts\activate.bat
-python -m pip -q install -r requirements.txt
---
+++ bootstrap.ps1
@@ -0,0 +1,4 @@
+If (-not (Test-Path ".venv")) { py -3.7 -m venv .venv --prompt md2bean }
+.venv\Scripts\activate.ps1
+python -m pip --quiet install --upgrade pip
+python -m pip --quiet install -r requirements.txt
