09ce2c0e7e5b746588f467d050e797960789cbea
Máté Farkas <mate.farkas@oneidentity.com>
2020-12-30 14:35:44 +0100

    Upgrade dependencies
    And remove beancount, fava as dependency,
    since they are not needed to run the script.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,4 @@
-If (-not (Test-Path ".venv")) { py -3.7 -m venv .venv --prompt md2bean }
+If (-not (Test-Path ".venv")) { py -3.8 -m venv .venv --prompt md2bean }
 .venv\Scripts\activate.ps1
 python -m pip --quiet install --upgrade pip
 python -m pip --quiet install -r requirements.txt
